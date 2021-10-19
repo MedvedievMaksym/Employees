c14689211b44c3a0687c206ebf6b3564ff623007
MedvedievM <medvediev.m91@gmail.com>
2021-10-19 19:06:43 +0300

    add filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,10 +11,12 @@ class App extends Component {
 		super(props);
 		this.state = {
 			data: [
-				{name: 'Andy R.', salary: 1900, increase: false, rise: true, id: 1},
-				{name: 'Lex D.', salary: 12000, increase: true, rise: false, id: 2},
-				{name: 'Max M.', salary: 15000, increase: false, rise: false, id: 3},
-			]
+				{name: 'Andy R.', salary: 900, increase: false, rise: true, id: 1},
+				{name: 'Lex D.', salary: 1200, increase: true, rise: false, id: 2},
+				{name: 'Max M.', salary: 1500, increase: false, rise: false, id: 3},
+			],
+			term: '',
+			filter: 'all'
 		}
 		this.maxId = 4;
 	}
@@ -57,11 +59,41 @@ class App extends Component {
 		}))
 	}
 
+	searchEmp = (items, term) => {
+		if(term.length === 0) {
+			return items;
+		}
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1;
+		})
+	}
+
+	onUpdateSearch = (term) => {
+		this.setState({term})
+	}
+
+	filterPost = (items, filter) => {
+		switch(filter) {
+			case 'rise':
+				return items.filter(item => item.rise);
+			case 'moreThen1000':
+				return items.filter(item => item.salary > 1000);
+			default:
+				return items;
+		}
+	}
+
+	onFilterSelect = (filter) => {
+		this.setState({filter});
+	}
+
 	render() {
 
+		const {data, term, filter} = this.state;
 		const employees = this.state.data.length;
 		//получим длинну нов массива с обьектами у которых increase === true
 		const increased = this.state.data.filter(item => item.increase).length;
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
 		return (
 			<div className="app">
@@ -71,16 +103,24 @@ class App extends Component {
 				/>
 
 				<div className="search-panel">
-					<SearchPanel />
-					<AppFilter />
+					<SearchPanel
+						onUpdateSearch={this.onUpdateSearch}
+					/>
+
+					<AppFilter
+						filter={filter}
+						onFilterSelect={this.onFilterSelect}
+					/>
 				</div>
 
 				<EmployeesList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItem}
 					onToggleProp={this.onToggleProp}
 				/>
-				<EmployeesAddForm onAdd={this.addItem} />
+				<EmployeesAddForm
+					onAdd={this.addItem}
+				/>
 			</div>
 		);
 	}
